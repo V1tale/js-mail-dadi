5ac8997f97a8724d7f77d7ccb6573199d395601b
V1tale <elativ.c@gmail.com>
2022-10-11 23:33:10 +0200

    completati Mail & Dice

--- dice/js/script.js
+++ dice/js/script.js
@@ -1,13 +1,16 @@
 const number = document.getElementById("number");
 number.addEventListener ("click", function () {
-const pcNumber = parseInt(Math.random() * (10));
+const pcNumber = parseInt(Math.random() * 5 + 1);
 console.log(pcNumber);
-const userNumber = parseInt(Math.random() * (10));
+const userNumber = parseInt(Math.random() * 5 + 1);
 console.log(userNumber);
+    console.log(userNumber);
     if (userNumber > pcNumber) {
-        alert("Hai vinto");
-    } else {
+        alert("Hai vinto!");
+    } else if (userNumber < pcNumber) {
         alert("hai perso!");
+    } else {
+        alert("Pareggio!");
     }
 
 const userOutput = document.getElementById("usernumber"). innerHTML = `Il tuo numero: ${userNumber}`;
